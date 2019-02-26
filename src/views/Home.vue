9539b7328b4931e5546b5caaec3c57f6c2b3fffa
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2019-02-26 22:28:13 +0000

    init(vue): create new app with vue-cli

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,18 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-4">
-        <Sources/>
-      </div>
-    </div>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import Sources from '@/components/Sources.vue'
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-export default {
-  name: 'sources',
+@Component({
   components: {
-    Sources
-  }
-}
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
 </script>
