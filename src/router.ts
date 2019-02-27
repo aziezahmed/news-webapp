dd1ed91782ef0a3d524d375b817e7e2ba62b5975
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2019-02-27 10:59:00 +0000

    feat(router): adding a route to the news page

--- src/router.ts
+++ src/router.ts
@@ -21,5 +21,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/news/:source',
+      name: 'news',
+      component: () => import('./views/News.vue'),
+      props: true,
+    },
   ],
 });
